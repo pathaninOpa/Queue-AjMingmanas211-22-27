3b5e967be3fd1fb209a68afe02bfd19ce24059e9
pathaninOpa <61925150+pathaninOpa@users.noreply.github.com>
2023-07-09 18:59:23 +0000

    fix & add

--- .ccls-cache/@home@runner@Queue-AjMingmanas211-22-27/node.h
+++ .ccls-cache/@home@runner@Queue-AjMingmanas211-22-27/node.h
@@ -1,11 +1,13 @@
 #ifndef node_h
 #define  node_h
+#define numMenu 3
 #include <iostream>
 using namespace std;
 
 class NODE{
 	 int order_number,qty;
-   char menu[10][10]={"ramen", "somtum"};
+   char menu[numMenu][20]={"ramen", "somtum", "chicken zaab"};
+   int pricelist[numMenu]={20,40,60};
 	NODE *nextPtr;
 public:
 	NODE(int,int=1);
@@ -13,21 +15,27 @@ public:
   int get_value();
 	void set_next(NODE *);
 	NODE* get_next();
+  void print_menu();
 };
 typedef NODE* NodePtr;
 
-NODE::NODE(int q,int o){
-	int qty=q;
-  int order_number = o;
+NODE::NODE(int o,int q){
+	qty=q;
+  order_number = o;
 	nextPtr=NULL;
 
 }
 int NODE::get_value(){
 	int price;
-  if(order_number>3 || order_number<1)
-  price= pricelist[order_number-1]*qty;
-  cout<<menu[order_number-1]<<endl;
-
+  if(order_number<=3 || order_number>=1){
+    price= pricelist[order_number-1]*qty;
+    cout<<menu[order_number-1]<<endl;
+    return price;
+  }
+  else{
+    cout<<"Wrong Order number..\n";
+    return 0;
+  }
 }
 
 NODE* NODE::get_next(){
@@ -41,9 +49,10 @@ void NODE::set_next(NODE *t){
 }
 
 NODE::~NODE(){
-	 cout<<"deleting " <<data<<endl;
-
+	 //cout<<"deleting " <<menu[order_number-1]<<endl;
 }
-
+void NODE::print_menu(){
+  cout<<menu[order_number-1]<<endl;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 using namespace std;
 #include "queue.h"
+#define numMenu 3
 
 int main(int argc,char *argv[]) {
 Queue q;
-int price,cash,change;
+
+  int price,cash,change,qc=0,itmc=0;
+  char menu[numMenu][20]={"ramen", "somtum", "chicken zaab"};
+  
   for(int i = 1; i<argc; i++){
     if (argv[i][0] == 'x'){
       price = q.dequeue();
+      cout<<"You have to pay "<<price<<" baht."<<endl;
       do {
         cout<<"Cash: ";
         cin >> cash;
       }
-      while (cash >= price);
-      cout<< "Thank you\n";
+      while (cash < price);
+      cout<< "Thank you _/\\_\n";
       if (cash > price){
-        cout << "The change is "<<cash - price <<endl;
+        cout << "The change is "<<cash - price<<" baht."<<endl;
       }
+      qc++; itmc--;
     }
     else{
       q.enqueue(atoi(argv[i]),atoi(argv[i+1]));
+      //cout<<menu[atoi(argv[i])-1]<<endl;
       cout << "Order received"<<endl;
-      i++;
+      i++; itmc++;
     }
   }
 
